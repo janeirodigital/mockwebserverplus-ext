53868e3e68e87600214fce2eac92d9553e8b1ecc
Justin Bingham <justin.bingham@janeirodigital.com>
2022-03-31 16:20:15 -0400

    bump versions

--- src/main/java/com/janeirodigital/mockwebserver/MockWebServerHelper.java
+++ src/main/java/com/janeirodigital/mockwebserver/MockWebServerHelper.java
@@ -3,17 +3,27 @@ package com.janeirodigital.mockwebserver;
 import okhttp3.mockwebserver.MockWebServer;
 
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class MockWebServerHelper {
 
     private MockWebServerHelper() { }
 
-    public static URL toUrl(MockWebServer server, String path) {
+    public static URL toMockUrl(MockWebServer server, String path) {
         try {
             return new URL(server.url(path).toString());
         } catch (MalformedURLException ex) {
             throw new IllegalStateException("Can't convert dispatcher request path to URL");
+        }
+    }
+
+    public static URI toMockUri(MockWebServer server, String path) {
+        try {
+            return toMockUrl(server, path).toURI();
+        } catch (URISyntaxException ex) {
+            throw new IllegalStateException("Can't convert dispatcher request path to URI");
         }
     }
 
